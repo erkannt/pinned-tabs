4b3e2bc06f075653b9b00c560712bc2e9a8524bc
Daniel Haarhoff <daniel@rknt.de>
2026-02-05 08:58:07 +0000

    Introduce menu

---
+++ popup.js
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded', function() {
+  const createBtn = document.getElementById('createBtn');
+  
+  createBtn.addEventListener('click', function() {
+    console.log('Create collection button clicked');
+  });
+});
